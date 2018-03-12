1f06c4424b9ffa2aa752c9b913ee09510896f526
John Huseinovic <john.huseinovic@mobivate.com>
2018-03-12 14:09:03 +0000

    fix php7

--- demo/Demo.php
+++ demo/Demo.php
@@ -5,10 +5,10 @@ require_once(dirname(__FILE__) . "/../bulksms/BulkSMS.php");
 
 
 // Defaults
-$USERNAME = "demo";
-$PASSWORD = "demo";
+$USERNAME = "facebook_10155227500195761";
+$PASSWORD = "e650cef62513b378d60e8311d2ca4f2c";
 
-$recipient = "61000000000";
+$recipient = "447932400495";
 $bulksms = new Blender\Client\BulkSMS();
 
 
@@ -16,7 +16,7 @@ $bulksms = new Blender\Client\BulkSMS();
 $bulksms->login($USERNAME, $PASSWORD);
 
 # Get Route ID
-$ROUTE_ID = $bulksms->getRouteIdByCountry("Australia");
+$ROUTE_ID = 'mglobal'; //$bulksms->getRouteIdByCountry("United Kingdom");
 
 // EXAMPLE 1 - Send to single recipient
 # Uncomment to run
@@ -25,15 +25,15 @@ $ROUTE_ID = $bulksms->getRouteIdByCountry("Australia");
 # ----------------------------------------------------------------------------------------------------------------
 
 // EXAMPLE 2 - Send same message to batch
-$batch = new Blender\Client\BatchMessageSingleBody();
-$batch->setOriginator("test");
-$batch->setRouteId($ROUTE_ID);
-$batch->setBody("Batch single <message> in 漢語");
-$batch->addMSISDN($recipient);
-// You can add as many as you like.
-
-# Schedule message for sending later
-$batch->setSchedule("2017-01-01T15:24:04", "Australia/Melbourne");
+//$batch = new Blender\Client\BatchMessageSingleBody();
+//$batch->setOriginator("test");
+//$batch->setRouteId($ROUTE_ID);
+//$batch->setBody("Batch single <message> in 漢語");
+//$batch->addMSISDN($recipient);
+//// You can add as many as you like.
+//
+//# Schedule message for sending later
+//$batch->setSchedule("2017-01-01T15:24:04", "Australia/Melbourne");
 
 # Dont filter duplicates - will send two messages to same recipient
 // $batch->addMSISDN($recipient);
@@ -57,20 +57,20 @@ $batch->setSchedule("2017-01-01T15:24:04", "Australia/Melbourne");
 
 // EXAMPLE 3 - Send batch of different messages to different recipients
 
-$batch = new Blender\Client\BatchMessageMultiBody();
-// Set defaults
-$batch->setOriginator("test");
-$batch->setRouteId($ROUTE_ID);
-$batch->setBody("Batch single message");
-
-$batch->addRecipient1($recipient);
-$batch->addRecipient2($recipient, "different message");
-$batch->addRecipient2($recipient, "different message with unicode טוקיו 東京(Tokyo)");
-$longtext = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse auctor turpis at nunc rutrum, eget sodales turpis molestie. Nullam mattis sit amet urna et tristique. Vivamus nec justo et dui sed. 200+ chars";
-$batch->addRecipient("orignew", $recipient, $longtext);
-# Uncomment to run
-//$responseXml =  $bulksms->sendBatch($batch);
-echo "Response: {$responseXml}\n";
+//$batch = new Blender\Client\BatchMessageMultiBody();
+//// Set defaults
+//$batch->setOriginator("test");
+//$batch->setRouteId($ROUTE_ID);
+//$batch->setBody("Batch single message");
+//
+//$batch->addRecipient1($recipient);
+//$batch->addRecipient2($recipient, "different message");
+//$batch->addRecipient2($recipient, "different message with unicode טוקיו 東京(Tokyo)");
+//$longtext = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse auctor turpis at nunc rutrum, eget sodales turpis molestie. Nullam mattis sit amet urna et tristique. Vivamus nec justo et dui sed. 200+ chars";
+//$batch->addRecipient("orignew", $recipient, $longtext);
+//# Uncomment to run
+////$responseXml =  $bulksms->sendBatch($batch);
+//echo "Response: {$responseXml}\n";
 
 # ----------------------------------------------------------------------------------------------------------------
 
@@ -88,6 +88,6 @@ $batch->setShortUrl(true);
 // -----------------------
 
 # Uncomment to run
-//$responseXml =  $bulksms->sendBatch($batch);
+$responseXml =  $bulksms->sendBatch($batch);
 echo "Response: {$responseXml}\n";
 
